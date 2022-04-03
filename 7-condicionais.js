9bd3d43fd31e5148ca1933328c9447ecc79422e1
Andreza Zeverino <you@example.com>
2022-04-03 18:53:58 -0300

    edição arquivo 7

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -1,6 +1,6 @@
 console.log("Trabalhando com condicionais");
 
-const listaDeDestinos = new Array (
+const listaDeDestinos = new Array(
     `Salvador`,
     `SP`,
     `RJ`
@@ -12,21 +12,43 @@ const estaAcompanhado = true;
 console.log("Destinos possíveis");
 console.log(listaDeDestinos);
 
+// if (idadeComprador >= 18) {
+//     console.log("Comprador maior de idade");
+//     listaDeDestinos.splice(1, 1);
+// } else {
+//     if (estaAcompanhado) {
+//         console.log("Comprador está acompanhado de um maior de idade");
+//         listaDeDestinos.splice(1, 1);
+
+//     } else {
+//         console.log("Você não conseguirá prosseguir com a compra por não possuir a idade mínima de 18 anos.")
+//     }
+
+// }
+
 if (idadeComprador >= 18) {
     console.log("Comprador maior de idade");
-    listaDeDestinos.splice(1,1);
-} else {
-    if(estaAcompanhado){
-        console.log("Comprador está acompanhado de um maior de idade");
-        listaDeDestinos.splice(1,1);
-
-    } else {
-        console.log("Você não conseguirá prosseguir com a compra por não possuir a idade mínima de 18 anos.")
-    }
-    
+    listaDeDestinos.splice(1, 1); // removendo item
+}   else if (estaAcompanhado == true) {
+    console.log("Comprador está acompanhado");
+    listaDeDestinos.splice(1, 1); //removedo item
+}else {
+    console.log("Não é maior de idade e não posso vender");
 }
 
-// abaixo deletei um registro, no caso o segundo da listagem (começa em 0). E o 1 significa que eu pedi pra exc só ele
+if (idadeComprador >= 18 || estaAcompanhado == true) { 
+    console.log("Comprador maior de idade");
+} else {
+    console.log("Não é maior de idade e não posso vender");
+}
+
+console.log("Embarque: \n\n")
+
+if(idadeComprador >= 18 && temPassagemComprada){
+    console.log("Boa viagem");
+} else{
+    console.log("Você não pode embarcar");
+} 
 
 console.log(listaDeDestinos);
 
--- README.md
+++ README.md
@@ -4,3 +4,5 @@ JS - Primeiros passos na linguagem - link: https://cursos.alura.com.br/course/ja
 Rodo no PowerShell
 
 Para rodar com node dou o comando node e seleciono o documento da pasta, aí aparecerá na tela o que quero, posso rodar no vs tbm
+
+Parei no capitulo 05 - Iterando sobre coleções - while 
